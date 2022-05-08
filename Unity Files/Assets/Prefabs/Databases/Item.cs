c24016e38fb14c3d9bdd269d6faae6b261f6d810
Gustavo Hinkel <gustavopradahinkel@gmail.com>
2022-05-08 08:04:43 -0300

    Added new assets and adjusted biome system

---
+++ Unity Files/Assets/Prefabs/Databases/Item.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public int id;
+    public string name;
+    public string description;
+    public Sprite texture;
+
+    public Item()
+    {
+        //
+    }
+}
