6a22d15c4f19e860adb756a07a227164cef8d845
Gustavo Hinkel <gustavopradahinkel@gmail.com>
2022-05-19 21:10:33 -0300

    Added waves to the water shader

--- Unity Files/Assets/HitController.cs
+++ Unity Files/Assets/HitController.cs
@@ -8,12 +8,35 @@ public class HitController : MonoBehaviour
     private Animator anim;
     private GameObject globalController;
 
+    public Sprite test;
+
+    private SpriteRenderer rend;
+    private GameObject player;
+
     private void Start()
     {
         physicCollider = this.transform.Find("Physic_Collider").GetComponent<BoxCollider2D>();
         touchCollider = this.transform.Find("Touch_Collider").GetComponent<BoxCollider2D>();
         anim = this.transform.Find("Skin").GetComponent<Animator>();
         globalController = GameObject.Find("Global Controller");
+
+        rend = this.transform.Find("Skin").GetComponent<SpriteRenderer>();
+        player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+        if (player.transform.position.x <= this.transform.position.x + rend.sprite.bounds.extents.x &&
+                player.transform.position.x >= this.transform.position.x - rend.sprite.bounds.extents.x &&
+                player.transform.position.y >= this.transform.position.y &&
+                player.transform.position.y <= this.transform.position.y + rend.sprite.bounds.size.y)
+        {
+            if (rend.color.a > .35f) rend.color = new Color(1, 1, 1, rend.color.a - .01f);
+        }
+        else
+        {
+            if (rend.color.a < 1) rend.color = new Color(1, 1, 1, rend.color.a + .01f);
+        }
     }
 
     public void Hit(int _toolStrength)
